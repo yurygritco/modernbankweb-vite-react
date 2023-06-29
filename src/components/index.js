009a0b0516d7169412d39e0db7a896c36b49d312
yurygritco <yurygritco@gmail.com>
2023-06-29 18:33:18 +0300

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,27 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedbackCard from "./FeedbackCard"
+import GetStarted from "./GetStarted"
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+
+export {
+  FeedbackCard,
+  Navbar,
+  GetStarted,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
